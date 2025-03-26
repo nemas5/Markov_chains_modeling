078935181ca4a338d1e469581bf98cac8c06a685
lockktar-ogar <lockktar.ogar@yandex.ru>
2025-03-26 12:46:30 +0300

    Another change

---
+++ Непрерывные цепи, задание 2/nepr2.py
@@ -0,0 +1,15 @@
+from typing import List
+import itertools as it
+from random import choices
+from collections import Counter
+from typing import Union
+
+import numpy as np
+from numpy.typing import NDArray as Arr
+import networkx as nx
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from scipy.integrate import simpson
+
+
+
